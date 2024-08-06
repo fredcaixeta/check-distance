3e5faea852e07e956d71ce8e41adf96ca99fbc86
fredcaixeta <frederico.caixeta@engenharia.ufjf.br>
2024-08-06 09:24:00 -0300

    English

--- app.py
+++ app.py
@@ -10,11 +10,6 @@ app = Flask(__name__)
 
 csv_file = 'distances.csv'
 
-if not os.path.exists(csv_file):
-    with open(csv_file, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Endereço 1","Endereço 2","Distância (km)"])
-
 @app.route('/')
 def home():
     return render_template(r'interface.html')
@@ -25,40 +20,34 @@ def calcular():
     end2 = request.form['endereco2']
     
     def calculo_enderecos(endereco1, endereco2):
-        try:
-            # Calcula a distancia entre os endereços
-            loc = geopy.Nominatim(user_agent="distance_calculator")
-            
-            loc1 = loc.geocode(endereco1, timeout=100)
-            loc1_lat = loc1.latitude
-            loc1_long = loc1.longitude
-            
-            loc2 = loc.geocode(endereco2, timeout=100)
-            loc2_lat = loc2.latitude
-            loc2_long = loc2.longitude
-            
-            dist = send_lat_long(loc1_lat, loc1_long, loc2_lat, loc2_long)
-            
-            return dist
-            
-        except Exception as e:
-            print(e)
-            raise TypeError
-            
+        
+        # Calcula a distancia entre os endereços
+        loc = geopy.Nominatim(user_agent="distance_calculator")
+        
+        loc1 = loc.geocode(endereco1, timeout=100)
+        loc1_lat = loc1.latitude
+        loc1_long = loc1.longitude
+        
+        loc2 = loc.geocode(endereco2, timeout=100)
+        loc2_lat = loc2.latitude
+        loc2_long = loc2.longitude
+        
+        dist = send_lat_long(loc1_lat, loc1_long, loc2_lat, loc2_long)
+        
+        return dist
             
     def send_lat_long(loc1_lat, loc1_long, loc2_lat, loc2_long):
-        if loc1_lat and loc2_lat:
-            distancia = geopy.distance.geodesic((loc1_lat, loc1_long), (loc2_lat, loc2_long)).km
-            
-            distancia = round(distancia, 2)
-            
-            # Save CSV
-            salva_csv(distancia)
-            
-            return distancia
-            #print(f"A distância entre {self.endereco1} e {self.endereco2} é de {distancia:.2f} km.")
-        else:
-            raise(ValueError)
+        
+        distancia = geopy.distance.geodesic((loc1_lat, loc1_long), (loc2_lat, loc2_long)).km
+        
+        distancia = round(distancia, 2)
+        
+        # Save CSV
+        salva_csv(distancia)
+        
+        return distancia
+        #print(f"A distância entre {self.endereco1} e {self.endereco2} é de {distancia:.2f} km.")
+    
         
     def salva_csv(dist):
         # Salva as informações no CSV
@@ -68,15 +57,20 @@ def calcular():
         
         return True
     
-    dist = calculo_enderecos(end1, end2)
-    #dist = "teste"
-    result = f"A distância entre {end1} e {end2} é de {dist} km."
+    try:
+        dist = calculo_enderecos(end1, end2)
+        #dist = "teste"
+        result = f"The distance between {end1} and {end2} is of {dist} km."
+        
+    except Exception as e:
+        print(e)
+        result = "Something went wrong - ERROR."
+        
     print(result)
     
     return render_template('result.html', result=result)
 
-
-@app.route('/distancias')
+@app.route('/distances')
 def distancias():
     distances = []
     try:
